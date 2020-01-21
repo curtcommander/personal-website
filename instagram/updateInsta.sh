c47e75bd172838ea4ef32ef38bf618fd107f457a
curtcommander <curtcommander@gmail.com>
2020-01-20 22:40:10 -0500

    fixed paths for instagram scraping

--- instagram/updateInsta.sh
+++ instagram/updateInsta.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 /var/www/html/instagram/scrapeInsta.py
-python3 /var/www/html/instagram/updateGallery.py
-python3 /var/www/html/instagram/updateIndex.py
+python3 /var/www/html/PersonalWebsite/instagram/scrapeInsta.py
+python3 /var/www/html/PersonalWebsite/instagram/updateGallery.py
+python3 /var/www/html/PersonalWebsite/instagram/updateIndex.py
