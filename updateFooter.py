94212f6344a6dd242ab2c4afc3ce728f369a6345
curtcommander <curtcommander@gmail.com>
2020-01-20 20:09:47 -0500

    minor bug fixes and added webd to projects page

--- updateFooter.py
+++ updateFooter.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 import datetime as dt
 import re
 
 current_year = str(dt.datetime.now().year)
 
+path = '/var/www/html/PersonalWebsite/'
 
-with open('footer.html', 'r') as f:
+with open(path+'footer.html', 'r') as f:
     html = f.read()
     
 tag = re.findall('<p id="copyright">.*?</p>', html)[0]
@@ -12,6 +14,6 @@ val = '\u00A9 Curt Commander | '+current_year
 tag_new = '<p id="copyright">'+val+'</p>'
 html = html.replace(tag, tag_new)
 
-with open('footer.html', 'w', encoding="utf-8") as f:
+with open(path+'footer.html', 'w', encoding="utf-8") as f:
     f.write(html)
     
