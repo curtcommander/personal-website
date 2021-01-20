f45097d06de147ad2f25d698f0393e65314ac8a9
curtcommander <curtcommander@gmail.com>
2021-01-20 16:18:15 -0500

    buildspec for aws CodePipeline

--- updateFooter.py
+++ updateFooter.py
@@ -4,9 +4,7 @@ import re
 
 current_year = str(dt.datetime.now().year)
 
-path = '/var/www/curtcommander.com/html/PersonalWebsite/html/'
-
-with open(path+'footer.html', 'r') as f:
+with open('html/footer.html', 'r') as f:
     html = f.read()
     
 tag = re.findall('<p id="copyright">.*?</p>', html)[0]
@@ -14,6 +12,6 @@ val = '\u00A9 Curt Commander | '+current_year
 tag_new = '<p id="copyright">'+val+'</p>'
 html = html.replace(tag, tag_new)
 
-with open(path+'footer.html', 'w', encoding="utf-8") as f:
+with open('html/footer.html', 'w', encoding="utf-8") as f:
     f.write(html)
     
